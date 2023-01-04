3b8ec026ed0ffe7ca9b2874fa68cbb1a31c4ce49
Özgenur Özkara <ozgenurozkara@Ozgenur-MacBook-Air.local>
2023-01-05 00:06:39 +0300

    search bar to the header components

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,24 +1,36 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import React from "react";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   return (
-    <div>
+    <div className="flex items-center justify-between max-w-6xl ">
       {/* Left */}
-      <div className="flex items-center justify-between max-w-6xl">
-        <div className=" cursor-pointer h-24 w-24 relative lg:inline-grid">
-          <Image
-            src="http://www.jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png"
-            fill
-            className="object-contain"
-          />
-        </div>
-        <h1>right sides</h1>
+      <div className=" cursor-pointer h-24 w-24 relative">
+        <Image
+          src="http://www.jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png"
+          alt="photo"
+          fill
+          className="object-contain"
+        />
       </div>
 
-      {/*Middle */}
+      {/* Middle */}
 
-      {/*Right */}
+      <div className="relative mt-1">
+        <div className="absolute top-2 left-2">
+          <FontAwesomeIcon icon={faSearch} className="h-5 text-gray-500" />
+        </div>
+        <input
+          type="text"
+          placeholder="Ara"
+          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+        />
+      </div>
+
+      {/* Right */}
+      <h1>right sides</h1>
     </div>
   );
 };
