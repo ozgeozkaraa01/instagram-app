77c87c0bb47195f5d22262f4ff23bde088cf3c8b
Özgenur Özkara <ozgenurozkara@gmail.com>
2023-01-06 17:06:08 +0300

    created the image and header sections of the post components

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,9 +1,22 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 
 const Post = ({ img, userImg, caption, username, id }) => {
   return (
-    <div>
-      <h1>{username}</h1>
+    <div className="bg-white my-7 border rounded-md">
+      {/* Post Header */}
+      <div className="flex items-center p-5">
+        <img
+          src={userImg}
+          alt={username}
+          className="h-12 rounded-full object-cover border p-1 mr-3"
+        />
+        <p className="font-bold">{username}</p>
+        <FontAwesomeIcon icon={faEllipsis} className="h-5" />
+      </div>
+      {/* Post Image */}
+      <img src={img} alt="photo" className="object-cover w-full"></img>
     </div>
   );
 };
