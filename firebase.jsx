6324b107c07d93667fbe82f4d916f687726de903
Özgenur Özkara <ozgenurozkara@gmail.com>
2023-01-11 11:18:03 +0300

    initialize the firebase

--- firebase.jsx
+++ firebase.jsx
@@ -1,17 +1,23 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCgCHgKsEej5ijJEGdl8K9Ee3MiKqUfOQs",
-  authDomain: "instagram-app-c4aa5.firebaseapp.com",
-  projectId: "instagram-app-c4aa5",
-  storageBucket: "instagram-app-c4aa5.appspot.com",
-  messagingSenderId: "986832453965",
-  appId: "1:986832453965:web:caf4536babb6c060f37704",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const storage = getStorage();
+
+export { app, db, storage };
