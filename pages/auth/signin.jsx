db3ee8ed6cafdf27aa7ade7c6838571b7e2ca8a2
Özgenur Özkara <ozgenurozkara@gmail.com>
2023-01-12 22:57:56 +0300

    updated signIn page

--- pages/auth/signin.jsx
+++ pages/auth/signin.jsx
@@ -1,17 +1,28 @@
 import React from "react";
-import { getProviders, signIn as SignIntoProvider } from "next-auth/react";
-import Header from "../../components/Header";
+import { getProviders, signIn } from "next-auth/react";
 
 const signin = ({ providers }) => {
   return (
     <>
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => SignIntoProvider(provider.id)}>
-            Sign in with {provider.name}
-          </button>
+      <div className="flex flex-col justify-center items-center space-x-7 mt-20">
+        <img
+          className="md:w-48"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png?20160616034027"
+          alt="photo"
+        ></img>
+        <div>
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button
+                onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+                className="text-white bg-[#1da1f2] hover:bg-[#1da1f2]/90 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55 mr-2 mb-2"
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </>
   );
 };
